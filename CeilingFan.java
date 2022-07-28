9a3008a9b9519ef7e81b69e9a140cffb8a508ffc
mattpotter234 <66799606+mattpotter234@users.noreply.github.com>
2022-07-28 10:04:46 -0400

    Add files via upload

---
+++ CeilingFan.java
@@ -0,0 +1,36 @@
+public class CeilingFan {
+
+    protected boolean isReversed;
+    protected int speed;
+
+    public CeilingFan() {
+        this.isReversed = false;
+        this.speed = 0;
+    }
+    
+    public void setSpeed(int s){
+        this.speed = s;
+    }
+    public int getSpeed(){
+        return speed;
+    }
+    public void setIsReversed(boolean r){
+        this.isReversed = r;
+    }
+    public boolean getIsReversed(){
+        return isReversed;
+    }
+    
+    // PullCord1
+    public void changeSpeed(){
+        // 3 speed settings
+        if(getSpeed()<3) setSpeed(getSpeed()+1);
+        // off    
+        else setSpeed(0);
+    }
+    // PullCord2
+    public void changeDirection(){
+        setIsReversed(!getIsReversed());
+    }
+
+}
