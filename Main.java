5df3ccfc8e8fb264a14b645da4a881e9e34bf1bb
mattpotter234 <66799606+mattpotter234@users.noreply.github.com>
2022-08-02 11:41:57 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        CeilingFan fan = new CeilingFan();
+    
+        System.out.println("Ceiling Fan Options:\n--------------------");
+        System.out.println("Pull a cord.\n1 - Speed\n2 - Direction\n0 - Exit");
+    
+        boolean exit = false;
+    
+        while(exit==false){
+            try{
+                String option = input.nextLine();
+        
+                switch(option){
+                    case "0":
+                        exit = true;
+                        break;
+                    case "1":
+                        fan.changeSpeed();
+                        break;
+                    case "2":
+                        fan.changeDirection();
+                        break;
+                    default:
+                     break;
+                }
+                System.out.println(fan.toString());
+            } catch(Exception e){
+                System.out.println("input unavailable.");
+                break;
+            }
+        }
+    }
+}
